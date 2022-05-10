2017607276343bd03725ab806ec2c389dd6d2602
Daniel <danelghisalin.mbida@ceff.ch>
2022-05-10 09:18:10 +0200

    watchAllUserBlocMaj

--- lib/application/watch_all_users_present/user_watch_all_bloc.dart
+++ lib/application/watch_all_users_present/user_watch_all_bloc.dart
@@ -1,10 +1,9 @@
 import 'dart:async';
-
 import 'package:apptest/domain/auth/user.dart';
 import 'package:apptest/domain/users/i_user_repository.dart';
 import 'package:apptest/domain/users/user_failure.dart';
-import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:kt_dart/collection.dart';
@@ -15,29 +14,26 @@ part 'user_watch_all_bloc.freezed.dart';
 
 @injectable
 class UserWatchAllBloc extends Bloc<UserWatchAllEvent, UserWatchAllState> {
-  
-  UserWatchAllBloc(this._userRepository) : super(const _Initial());
-    final IUserRepository _userRepository;
-  StreamSubscription<Either<UserFailure, KtList<User>>>?
-      _userStreamSubscription;
+  final IUserRepository _userRepository;
+  UserWatchAllBloc(this._userRepository) : super(const _Initial()) {
+    StreamSubscription<Either<UserFailure, KtList<User>>>?
+        _userStreamSubscription;
 
-  @override
-  Stream<UserWatchAllState> mapEventToState(UserWatchAllEvent event) async* {
-    yield* event.map(
-      watchAllStarted: (e) async*{
-        yield const UserWatchAllState.loadInProgress();
-        await _userStreamSubscription?.cancel();
-         _userStreamSubscription= _userRepository.watchAllUsersPresent().listen(
-          (failureOrUsers) =>add(UserWatchAllEvent.userReceived(failureOrUsers)),
-        );
-      }, 
-      userReceived: (e) async*{
-         yield e.failureOrUsers.fold(
+    on<_WatchAllStarted>((event, emit) async {
+      emit(const UserWatchAllState.loadInProgress());
+      await _userStreamSubscription?.cancel();
+      _userStreamSubscription = _userRepository.watchAllUsersPresent().listen(
+            (failureOrUsers) =>
+                add(UserWatchAllEvent.userReceived(failureOrUsers)),
+          );
+    });
+    on<_UsersReceived>((event, emit) {
+      emit(
+        event.failureOrUsers.fold(
           (f) => UserWatchAllState.loadFailure(f),
           (users) => UserWatchAllState.loadSuccess(users),
-        );
-      },
+        ),
       );
+    });
   }
-  
 }
