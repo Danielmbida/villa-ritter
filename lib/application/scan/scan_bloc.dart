5c5967a1bf0d4723243dc972196565b3dfaeb244
Daniel <danelghisalin.mbida@ceff.ch>
2022-05-10 08:48:51 +0200

    blocScanMaj

--- lib/application/scan/scan_bloc.dart
+++ lib/application/scan/scan_bloc.dart
@@ -1,37 +1,28 @@
-import 'dart:async';
-
 import 'package:apptest/domain/scan/value_object.dart';
-import 'package:bloc/bloc.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
-import 'package:meta/meta.dart';
-
 part 'scan_bloc.freezed.dart';
 part 'scan_event.dart';
 part 'scan_state.dart';
 
 @injectable
 class ScanBloc extends Bloc<ScanEvent, ScanState> {
-  ScanBloc() : super(const ScanState.initial());
-
-  @override
-  Stream<ScanState> mapEventToState(ScanEvent event,) async* {
-    yield* event.map(
-      scanCheckRequested: (scanData) async*{
-        final codeQR = CodeQR(scanData.codeQR).value;
-        yield codeQR.fold((f) => const ScanState.scanFail(), (data){
-          return ScanState.sucess(scanData.codeQR);
-        });
-      },
-      scanDisplayErrorRequested: (_) async*{
-        yield const ScanState.scanTimeout();
-      },
-      scanResetted: (_) async*{
-        yield const ScanState.initial();
-      },
-      
+  ScanBloc() : super(const ScanState.initial()) {
+    on<_ScanCheckRequested>((event, emit) {
+      final codeQR = CodeQR(event.codeQR).value;
+      emit(
+        codeQR.fold((f) => const ScanState.scanFail(), (data) {
+          return ScanState.sucess(event.codeQR);
+        }),
       );
+    });
+
+    on<_ScanDisplaErrorRequested>((event, emit) {
+      emit(const ScanState.scanTimeout());
+    });
+    on<_ScanResetted>((event, emit) {
+      emit(const ScanState.initial());
+    });
   }
 }
--- lib/application/user_watcher_me/user_watcher_me_bloc.dart
+++ lib/application/user_watcher_me/user_watcher_me_bloc.dart
@@ -1,14 +1,12 @@
 import 'dart:async';
-
 import 'package:apptest/domain/auth/user.dart';
 import 'package:apptest/domain/users/i_user_repository.dart';
 import 'package:apptest/domain/users/user_failure.dart';
-import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:kt_dart/kt.dart';
-
 part 'user_watcher_me_event.dart';
 part 'user_watcher_me_state.dart';
 part 'user_watcher_me_bloc.freezed.dart';
