45e4564cca234aa94d0ab7ad2ca1ebe278088d08
daniel <danielghislain.mbida@ceff.ch>
2022-05-31 09:55:14 +0200

    Auto stash before merge of "master" and "origin/master"

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:apptest/presentation/core/app_widget.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
+// ignore: always_use_package_imports
 import 'injection.dart';
 
 Future<void> main() async {
