8639082ba0c8d8231bc4d58b6a420d3243d6ab58
Daniel <danelghisalin.mbida@ceff.ch>
2022-06-02 14:02:06 +0200

    safearea

--- lib/presentation/core/users/infosDialog/user_infos_dialog.dart
+++ lib/presentation/core/users/infosDialog/user_infos_dialog.dart
@@ -26,131 +26,133 @@ class UserInfosDialog extends StatelessWidget {
       ).animate(animation),
       child: Opacity(
         opacity: animation.value,
-        child: Scaffold(
-          backgroundColor: Colors.white70,
-          body: SizedBox(
-            height: MediaQuery.of(context).size.height,
-            child: Column(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.23,
-                  child: Card(
-                    shape: const RoundedRectangleBorder(
-                      side: BorderSide(color: Colors.white70),
-                      borderRadius: BorderRadius.only(
-                        bottomRight: Radius.circular(130),
-                      ),
-                    ),
-                    color: Theme.of(context).primaryColor,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        IconButton(
-                          icon: const Icon(
-                            Icons.close,
-                            color: Colors.white,
-                          ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
+        child: SafeArea(
+          child: Scaffold(
+            backgroundColor: Colors.white70,
+            body: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.23,
+                    child: Card(
+                      shape: const RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.white70),
+                        borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(130),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 15,
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                AppLocalizations.of(context)!.infos_for_string,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize:
-                                      MediaQuery.of(context).size.width * 0.10,
-                                ),
-                              ),
-                              Text(
-                                user.name,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize:
-                                      MediaQuery.of(context).size.width * 0.10,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Column(
-                  children: [
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.025, //20
-                    ),
-                    UserInfoItemForm(
-                      field: AppLocalizations.of(context)!.name_string,
-                      infos: user.name,
-                      icon: Icons.person_outline_outlined,
-                      iconColor: Colors.yellow,
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.025,
-                    ),
-                    InkWell(
-                      onTap: () {
-                        UrlLauncher.launchURL(
-                          "https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSBpDwRQrKjcXntRkSnvpNmhFQSRJMKhtCrmTpKKbSfNhKWFGxNBxfqTcCwQGwcChbCgGQCS",
-                          "mailto://${user.email}",
-                        );
-                      },
-                      child: UserInfoItemForm(
-                        field: AppLocalizations.of(context)!.email_string,
-                        infos: user.email,
-                        icon: Icons.email_outlined,
-                        iconColor: Colors.blue,
                       ),
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.025,
-                    ),
-                    UserInfoItemForm(
-                      field: AppLocalizations.of(context)!.birthday_string,
-                      infos: user.birthDate,
-                      icon: FontAwesomeIcons.birthdayCake,
-                      iconColor: Colors.pink,
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.025,
-                    ),
-                    UserInfoItemForm(
-                      field: AppLocalizations.of(context)!.gender_string,
-                      infos: _userGender(user.genre, context),
-                      icon: FontAwesomeIcons.transgenderAlt,
-                      iconColor: Colors.black,
-                    ),
-                  ],
-                ),
-                Expanded(
-                  child: CircleAvatar(
-                    radius: MediaQuery.of(context).size.width * 0.07,
-                    backgroundColor: Theme.of(context).primaryColor,
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Icon(
-                        Icons.close,
-                        size: MediaQuery.of(context).size.width * 0.06,
-                        color: Colors.white,
+                      color: Theme.of(context).primaryColor,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          IconButton(
+                            icon: const Icon(
+                              Icons.close,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 15,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  AppLocalizations.of(context)!.infos_for_string,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize:
+                                        MediaQuery.of(context).size.width * 0.10,
+                                  ),
+                                ),
+                                Text(
+                                  user.name,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize:
+                                        MediaQuery.of(context).size.width * 0.10,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                ),
-              ],
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.025, //20
+                      ),
+                      UserInfoItemForm(
+                        field: AppLocalizations.of(context)!.name_string,
+                        infos: user.name,
+                        icon: Icons.person_outline_outlined,
+                        iconColor: Colors.yellow,
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.025,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          UrlLauncher.launchURL(
+                            "https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSBpDwRQrKjcXntRkSnvpNmhFQSRJMKhtCrmTpKKbSfNhKWFGxNBxfqTcCwQGwcChbCgGQCS",
+                            "mailto://${user.email}",
+                          );
+                        },
+                        child: UserInfoItemForm(
+                          field: AppLocalizations.of(context)!.email_string,
+                          infos: user.email,
+                          icon: Icons.email_outlined,
+                          iconColor: Colors.blue,
+                        ),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.025,
+                      ),
+                      UserInfoItemForm(
+                        field: AppLocalizations.of(context)!.birthday_string,
+                        infos: user.birthDate,
+                        icon: FontAwesomeIcons.birthdayCake,
+                        iconColor: Colors.pink,
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.025,
+                      ),
+                      UserInfoItemForm(
+                        field: AppLocalizations.of(context)!.gender_string,
+                        infos: _userGender(user.genre, context),
+                        icon: FontAwesomeIcons.transgenderAlt,
+                        iconColor: Colors.black,
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: CircleAvatar(
+                      radius: MediaQuery.of(context).size.width * 0.07,
+                      backgroundColor: Theme.of(context).primaryColor,
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Icon(
+                          Icons.close,
+                          size: MediaQuery.of(context).size.width * 0.06,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
