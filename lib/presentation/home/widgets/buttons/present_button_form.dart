79d9ea61b7af81d061da6762b9c03cb830677a31
Daniel <danielghislain.mbida@ceff.ch>
2022-06-15 20:27:19 +0200

    Scanner unique quand la villa est ouverte

--- lib/presentation/home/widgets/buttons/present_button_form.dart
+++ lib/presentation/home/widgets/buttons/present_button_form.dart
@@ -1,7 +1,10 @@
+import 'package:apptest/application/force_hour/watch/watch_force_hour_bloc.dart';
+import 'package:apptest/application/horaire/horaire_cubit.dart';
 import 'package:apptest/domain/auth/user.dart';
 import 'package:apptest/presentation/routes/router.gr.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PrensentButtonForm extends StatelessWidget {
   const PrensentButtonForm({
@@ -14,24 +17,52 @@ class PrensentButtonForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double mediaHeight = MediaQuery.of(context).size.height;
-    return InkWell(
-      onTap: () {
-        context.router.push(const ScanRoute());
-      },
-      child: Card(
-        color: Theme.of(context).primaryColor,
-        child: SizedBox(
-          child: Column(
-            children: [
-              Icon(
-                Icons.qr_code,
-                size: mediaHeight * .090,
-                color: Colors.white,
+    bool villaIsClose = false;
+    return BlocBuilder<WatchForceHourBloc, WatchForceHourState>(
+      builder: (context, state) {
+        state.maybeMap(
+          orElse: () {},
+          loadSuccess: (state) {
+            if (state.hours[0].close) {
+              villaIsClose = true;
+            }
+          },
+        );
+        return BlocListener<HoraireCubit, HoraireState>(
+          listener: (context, state) {
+            state.map(
+              ouvert: (_) {
+                villaIsClose = false;
+              },
+              ferme: (_) {
+                villaIsClose = true;
+              },
+            );
+          },
+          child: InkWell(
+            onTap: () {
+              if (villaIsClose == false) {
+                context.router.push(const ScanRoute());
+                print("ouvert");
+              }
+            },
+            child: Card(
+              color: Theme.of(context).primaryColor,
+              child: SizedBox(
+                child: Column(
+                  children: [
+                    Icon(
+                      Icons.qr_code,
+                      size: mediaHeight * .090,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
